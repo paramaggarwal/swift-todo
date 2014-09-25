212d403062d35dd6f388d93f0862e9ee73de7f68
Param Aggarwal <paramaggarwal@gmail.com>
2014-09-25 11:34:48 +0530

    TableView prints 1 to 5.

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("item", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = String(indexPath.row)
+        
+        return cell;
+        
+    }
 }
 
